fc8e50bd2b6a090909a944b01b27acc6956783e7
Fredrik Enestad <fredrik@enestad.com>
2019-12-16 11:00:10 +0100

    add package info

--- mix.exs
+++ mix.exs
@@ -4,25 +4,28 @@ defmodule OCPropagationStackdriver.MixProject do
   def project do
     [
       app: :oc_propagation_stackdriver,
+      description: "OpenCensus header propagation for Stackdriver/GCP",
       version: "0.1.0",
       elixir: "~> 1.9",
-      start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+      package: package()
     ]
   end
 
-  # Run "mix help compile.app" to learn about applications.
   def application do
     [
       extra_applications: [:logger]
     ]
   end
 
-  # Run "mix help deps" to learn about dependencies.
-  defp deps do
-    [
-      # {:dep_from_hexpm, "~> 0.3.0"},
-      # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
-    ]
+  defp package do
+    %{
+      licenses: ["MIT"],
+      links: %{
+        "GitHub" => "https://github.com/soundtrackyourbrand/oc-propagation-stackdriver"
+      }
+    }
   end
+
+  defp deps, do: []
 end
